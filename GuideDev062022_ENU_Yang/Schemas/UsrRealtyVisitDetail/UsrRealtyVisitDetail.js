8b5952df1f6b9fb53dea03547fe757ac655196dd
{youeitetsu} <{ray.1212@163.com}>
2022-07-26 00:43:17 +0900

    RealtyVisit, Validation, calculCommission added

---
+++ GuideDev062022_ENU_Yang/Schemas/UsrRealtyVisitDetail/UsrRealtyVisitDetail.js
@@ -0,0 +1,8 @@
+define("UsrRealtyVisitDetail", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyVisit",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
---
+++ GuideDev062022_ENU_Yang/Schemas/UsrRealtyVisitPage/UsrRealtyVisitPage.js
@@ -0,0 +1,68 @@
+define("UsrRealtyVisitPage", [], function() {
+	return {
+		entitySchemaName: "UsrRealtyVisit",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "DATETIME4d93f6c7-0a05-4d96-83c3-b99f978c86cc",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrVisitDatetime",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LOOKUPf59a67fe-5276-48c5-83c8-ca51c0c2bd8c",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrPotentialCustomer",
+					"enabled": true,
+					"contentType": 5
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "STRINGd3b8e784-f95f-4d21-8887-f9e9e814b66c",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "UsrComment",
+					"enabled": true
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
